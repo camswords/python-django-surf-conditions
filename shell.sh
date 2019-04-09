7fb18a673cc317bcb28f23e464f05373ffe96532
Cam Swords <cam.swords@gmail.com>
2019-04-09 12:55:38 +1000

    added a useful script to create a django shell directly from a docker entrypoint

--- shell.sh
+++ shell.sh
@@ -4,4 +4,4 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 cd $BASEDIR
 source ./linux_venv/bin/activate
-MAGIC_SEAWEED_API_KEY=02bbdcc179bb7e02d7903fce5dec92e3 python manage.py runserver 0.0.0.0:8000
+MAGIC_SEAWEED_API_KEY=02bbdcc179bb7e02d7903fce5dec92e3 python manage.py shell
