482ed63c14fe3e550a832d68a84445f63eb89b58
Cam Swords <cam.swords@gmail.com>
2019-04-11 16:58:18 +1000

    the entire row in the more reports table is clickable

--- surf/static/surf/script.js
+++ surf/static/surf/script.js
@@ -1,2 +1,12 @@
 
 console.log("Welcome to the Surf Report!");
+
+let rows = document.querySelectorAll(".more_reports tr");
+
+rows.forEach((row) => {
+    let url = row.getAttribute('data-url');
+
+    if (url) {
+        row.onclick = () => window.location.href = url;
+    }
+});
