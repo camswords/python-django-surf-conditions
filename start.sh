1291df55a9e4c72d85ccbcda6942068310a026c7
Cam Swords <cam.swords@gmail.com>
2019-04-13 15:17:57 +1000

    removed shell scripts from the project

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-
-cd $BASEDIR
-source ./linux_venv/bin/activate
-MAGIC_SEAWEED_API_KEY=02bbdcc179bb7e02d7903fce5dec92e3 python manage.py runserver 0.0.0.0:8000
