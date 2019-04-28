3685de083eb78536731e4d5947a4fc63270cfbf3
Cam Swords <cam.swords@gmail.com>
2019-04-28 17:50:59 +1000

    added the basics to access the memcached instance

---
+++ surf/infrastructure.py
@@ -0,0 +1,39 @@
+import json
+
+from pymemcache.client.base import Client
+
+from . import settings
+
+
+class CacheError(Exception):
+    pass
+
+
+class JsonSerialisation:
+    def serialise(self, key, value):
+        if len(key) > 250:
+            raise CacheError('cache key is longer than 250 chars, memcached wont store it. key is %s' % key)
+
+        try:
+            return json.dumps(value)
+        except Exception as ex:
+            raise CacheError('failed to serialise key %s for storing in cache' % key) from ex
+
+
+    def deserialise(self, key, value):
+        try:
+            return json.loads(value)
+        except Exception as ex:
+            raise CacheError('failed to deserialise key %s for retrieval from cache' % key) from ex
+
+
+class Cache:
+    def __init__(self,
+                 connect_timeout=settings.MEMCACHED_SERVER_CONNECT_TIMEOUT_SECONDS,
+                 timeout=settings.MEMCACHED_SERVER_READ_TIMEOUT_SECONDS,
+                 serialisation_strategy=JsonSerialisation()):
+        self._cache = Client(settings.MEMCACHED_SERVER_URI,
+                             serializer=serialisation_strategy.serialise,
+                             deserializer=serialisation_strategy.deserialise,
+                             connect_timeout=connect_timeout,
+                             timeout=timeout)
--- surf/settings.py
+++ surf/settings.py
@@ -2,9 +2,16 @@ import os
 
 from django.conf import settings
 
-SEARCH_RESULTS_PER_PAGE = int(getattr(settings, 'SEARCH_RESULTS_PER_PAGE', os.getenv('SEARCH_RESULTS_PER_PAGE', '14')))
-MAGIC_SEAWEED_API_KEY = getattr(settings, 'SURF_MAGICSEAWEED_API_KEY', os.getenv('MAGIC_SEAWEED_API_KEY', None))
-MAGIC_SEAWEED_MANLY_NSW_SPOT_ID = '524'
-MAGIC_SEAWEED_URL = 'http://magicseaweed.com/api/{0}/forecast/?spot_id={1}'.format(MAGIC_SEAWEED_API_KEY, MAGIC_SEAWEED_MANLY_NSW_SPOT_ID)
 
-API_SURF_REPORTS_PER_PAGE = int(getattr(settings, 'API_SURF_REPORTS_PER_PAGE', os.getenv('API_SURF_REPORTS_PER_PAGE', '3')))
+def get_setting(name, default_value, translation_fn=lambda x: x):
+    return translation_fn(getattr(settings, name, os.getenv(name, default_value)))
+
+
+API_SURF_REPORTS_PER_PAGE = get_setting('API_SURF_REPORTS_PER_PAGE', '3', int)
+MEMCACHED_SERVER_URI = get_setting('MEMCACHED_SERVER_URI', 'localhost:11211')
+MEMCACHED_SERVER_CONNECT_TIMEOUT_SECONDS = get_setting('MEMCACHED_SERVER_CONNECT_TIMEOUT_SECONDS', '5', int)
+MEMCACHED_SERVER_READ_TIMEOUT_SECONDS = get_setting('MEMCACHED_SERVER_READ_TIMEOUT_SECONDS', '5', int)
+MAGIC_SEAWEED_API_KEY = get_setting('SURF_MAGICSEAWEED_API_KEY', None)
+MAGIC_SEAWEED_MANLY_NSW_SPOT_ID = get_setting('MAGIC_SEAWEED_MANLY_NSW_SPOT_ID', '524')
+MAGIC_SEAWEED_URL = 'http://magicseaweed.com/api/%s/forecast/?spot_id=%s' % (MAGIC_SEAWEED_API_KEY, MAGIC_SEAWEED_MANLY_NSW_SPOT_ID)
+SEARCH_RESULTS_PER_PAGE = get_setting('SEARCH_RESULTS_PER_PAGE', '14', int)
